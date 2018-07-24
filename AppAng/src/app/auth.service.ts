ddb59b725072932de3d33d2b3fa16fbbc61f59a3
unknown <hmaididisoufien@yahoo.fr>
2018-07-24 20:02:42 +0200

    Register and Login with cloud Mlab

--- AppAng/src/app/auth.service.ts
+++ AppAng/src/app/auth.service.ts
@@ -6,9 +6,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
   private UrlRegister = 'http://localhost:3000/api/register';
+  private UrLogin = 'http://localhost:3000/api/login';
+
   constructor( private http: HttpClient) { }
 
   UserEntregister(user) {
    return this.http.post(this.UrlRegister, user);
+  }
+
+  UserLogin(userObjet) {
+    return this.http.post(this.UrLogin, userObjet);
+
   }
 }
--- AppAng/src/app/login/login.component.ts
+++ AppAng/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+   loginUserData = { };
+  constructor( private serviceLogin: AuthService) { }
 
   ngOnInit() {
   }
+  LoginUser() {
+     console.log(this.loginUserData);
+     this.serviceLogin.UserLogin(this.loginUserData)
+     .subscribe(
+       res => console.log(res),
+       err => console.error()
+     );
 
+  }
 }
